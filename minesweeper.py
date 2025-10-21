b93f0f6f465965935a7e71902afc24133f4ee8b9
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-10-21 12:48:13 -0700

    replace placeholder mine sprite

--- minesweeper.py
+++ minesweeper.py
@@ -107,17 +107,23 @@ def onMousePress(x, y):
         if topSquares[i].contains(x, y):
             topSquares[i].visible = False
             if mines[i]:
-                Label('boom', squares[i].centerX, squares[i].centerY, size=20, fill=Colors.red)
+                #Label('boom', squares[i].centerX, squares[i].centerY, size=20, fill=Colors.red)
+                Assets.mine(
+                    squares[i].centerX, 
+                    squares[i].centerY, 
+                    scale=0.11, 
+                    doRandomRotation=True
+                )
             elif numbers[i] > 0:
-                numberLabel = Label(str(numbers[i]), squares[i].centerX, squares[i].centerY, size=20)
-                if numberLabel.value == "1": numberLabel.fill = Colors.number.one
-                elif numberLabel.value == "2": numberLabel.fill = Colors.number.two
-                elif numberLabel.value == "3": numberLabel.fill = Colors.number.three
-                elif numberLabel.value == "4": numberLabel.fill = Colors.number.four
-                elif numberLabel.value == "5": numberLabel.fill = Colors.number.five
-                elif numberLabel.value == "6": numberLabel.fill = Colors.number.six
-                elif numberLabel.value == "7": numberLabel.fill = Colors.number.seven
-                elif numberLabel.value == "8": numberLabel.fill = Colors.number.eight
+                numberLabel = Label(numbers[i], squares[i].centerX, squares[i].centerY, size=20)
+                if numberLabel.value == 1: numberLabel.fill = Colors.number.one
+                elif numberLabel.value == 2: numberLabel.fill = Colors.number.two
+                elif numberLabel.value == 3: numberLabel.fill = Colors.number.three
+                elif numberLabel.value == 4: numberLabel.fill = Colors.number.four
+                elif numberLabel.value == 5: numberLabel.fill = Colors.number.five
+                elif numberLabel.value == 6: numberLabel.fill = Colors.number.six
+                elif numberLabel.value == 7: numberLabel.fill = Colors.number.seven
+                elif numberLabel.value == 8: numberLabel.fill = Colors.number.eight
                 else: numberLabel.fill = None
             else: #clearing an area (no 0 squares next to a unrevealed square)
                 for j in [-1, 1, -cols, cols, -cols-1, -cols+1, cols-1, cols+1]: #checks the fucking surrounding squares
